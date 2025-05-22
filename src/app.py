770713d249062310c2ee09118909dbc5faa37f41
Sxg0673 <santiago.castillo@uao.edu.co>
2025-05-22 05:47:41 -0500

    Agrega librerias en app.py

---
+++ src/app.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import messagebox
+
+# Importamos todas las funciones necesarias desde logica.py
+from logica import (
+    registrar_proyecto,
+    cargar_proyectos,
+    ver_proyecto,
+    eliminar_proyecto
+)
+
+class App(tk.Tk):
+    def __init__(self):
+        pass
+
